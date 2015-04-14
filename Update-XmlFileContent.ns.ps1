bd53bb5a020e7a03512932398466dafbd2ca9e8c
Yang Du <yangdu@thoughtworks.com>
2015-04-14 13:38:28 +0800

    Add Timestemp

--- Update-XmlFileContent.ns.ps1
+++ Update-XmlFileContent.ns.ps1
@@ -22,6 +22,13 @@ $config = @{
 
 }
 
+$root = $MyInvocation.MyCommand.Path | Split-Path -parent
+$envName = $args[0]
+$versionNumber = $args[1]
+$timestamp = [DateTime]::now.ToString("yyyy.MM.dd.HH.mm.ss.FFF")
+
+
+
 Update-XmlFileContent "$sourcePath\Web.config" {
             $_.SelectSingleNode("//appSettings/add[@key='DBConnectionString']").value = "Data Source=$($config.DbServer);Initial Catalog=$($config.DbName);Integrated Security=True;" 
 }
