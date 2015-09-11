915ef55b343d0e19155d6067121962ce383ae47c
Yang Du <yangdu@thoughtworks.com>
2015-09-11 17:42:22 +0800

    [dy] Add a script to create test.txt

---
+++ MakeReadyTXT.ps1
@@ -0,0 +1,28 @@
+#Get-ChildItem -Path D:\IIS -Filter test.txt -Recurse | % {$_.fullname} | Measure-Object
+#cat .\test.txt | % {if (-Not (Test-Path($_))) { New-Item $_ -type file }}
+
+$env = "test"
+$fileName = "test.txt"
+$counter = 0
+$readyFiles = @( 
+                "D:\IIS\WebUI-$env\", `
+                "D:\IIS\WebAPI-$env\", `
+                "D:\IIS\WebAdmin-$env\"
+)
+
+
+ForEach ($folder in $readyFiles) 
+{
+    $file = $folder + $filename
+ 
+    if (-Not (Test-Path ($file) ) -and (Test-Path ($folder) )) {
+        write-host "$file created"
+        $counter = $counter + 1
+        New-Item $file -type file
+    }
+#
+}
+
+invoke-command {iisreset}
+
+write-host "$counter file created."
